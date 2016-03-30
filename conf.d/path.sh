5f664a9f1960585a263435ef36b24592c186754c
Liam O'Boyle <liam@ontheroad.net.nz>
2016-03-30 11:59:31 +1100

    Add rbenv stuff to the path.

--- conf.d/path.sh
+++ conf.d/path.sh
@@ -1,2 +1,5 @@
 export PATH=${PATH}:~/bin:~/.composer/vendor/bin:~/.local/bin
 
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
